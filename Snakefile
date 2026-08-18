38dfbd412f98655dea892073336c7e79f1cbbab3
Dazhong Xia <dazhong.xia@catalyst.coop>
2026-08-18 14:25:24 -0400

    feat: remote read/write for stub supply curve pipeline
    Still need to configure this to use the Datastore code instead of raw HTTP reads.

--- Snakefile
+++ Snakefile
@@ -1,7 +1,22 @@
+envvars:
+  "CLOUDFLARE_R2_ENDPOINT",
+  "CLOUDFLARE_R2_ACCESS_KEY_ID",
+  "CLOUDFLARE_R2_SECRET_ACCESS_KEY",
+
+storage http:
+  provider="http",
+  retrieve=False
+
+storage r2:
+  provider="s3",
+  endpoint_url=os.environ["CLOUDFLARE_R2_ENDPOINT"],
+  access_key=os.environ["CLOUDFLARE_R2_ACCESS_KEY_ID"],
+  secret_key=os.environ["CLOUDFLARE_R2_SECRET_ACCESS_KEY"]
+
 rule supply_curve:
-  input:
-    "SupplyCurve.csv"
+  input: 
+    storage.http("https://raw.githubusercontent.com/EIAgov/BlueSky/refs/heads/main/input/electricity/cem_inputs/SupplyCurve.csv")
   output:
-    "supply_curve.csv"
+    storage.r2(f"s3://test-catalyst-coop/supply_curve.csv")
   script:
     "src/cnems_inputs/stub_supply_curve.py"
--- src/cnems_inputs/stub_supply_curve.py
+++ src/cnems_inputs/stub_supply_curve.py
@@ -1,29 +1,45 @@
+"""Stub transformation pipeline for SupplyCurve.csv.
+
+Reads a file from GitHub (soon to be Datastore...), then publishes it to R2.
+"""
+
 from pathlib import Path
-from typing import TYPE_CHECKING
 
 import polars as pl
 
-if TYPE_CHECKING:
-    from snakemake.iocontainers import snakemake
-
 
 def extract(input_path: Path) -> pl.LazyFrame:
-    print(f"reading from {input_path}")
+    """Make a LazyFrame from the input path.
+
+    Note that we've set the HTTP storage engine to `retrieve=False`. This means
+    that *we* handle the remote fetching with Polars, rather than having Snakemake
+    handle remote fetching to a local cached location. This allows us to have
+    partial remote reads using Polars predicate/projection pushdown.
+    """
     return pl.scan_csv(input_path)
 
 
 def transform(raw: pl.LazyFrame) -> pl.LazyFrame:
+    """Do nothing."""
     return raw
 
 
 def load(transformed: pl.LazyFrame, output_path: Path) -> None:
+    """Write LazyFrame to output.
+
+    Note that here, we've set the S3 storage engine to `retrieve=True`. This
+    means that we write to a local cached path, and the S3 storage plugin will read
+    from that path and write to the configured location.
+    """
     transformed.sink_csv(output_path)
 
 
 def run(input_path, output_path):
-    raw = extract(input_path)
-    transformed = transform(raw)
-    load(transformed, output_path)
+    """E, T, L."""
+    load(transform(extract(input_path)), output_path)
 
 
-run(snakemake.input[0], snakemake.output[0])
+if __name__ == "__main__":
+    from snakemake.iocontainers import snakemake
+
+    run(snakemake.input[0], snakemake.output[0])
