1e6b484d16aa574ba0de73832a595a80c157c1d4
Dazhong Xia <dazhong.xia@catalyst.coop>
2026-08-20 15:41:19 -0400

    feat: fully utilize Datastore caching mechanism

--- Snakefile
+++ Snakefile
@@ -3,10 +3,6 @@ envvars:
   "CLOUDFLARE_R2_ACCESS_KEY_ID",
   "CLOUDFLARE_R2_SECRET_ACCESS_KEY",
 
-storage http:
-  provider="http",
-  retrieve=False
-
 storage r2:
   provider="s3",
   endpoint_url=os.environ["CLOUDFLARE_R2_ENDPOINT"],
@@ -16,13 +12,13 @@ storage r2:
 def datastore_resources(dataset, **filters):
     from cnems_inputs.datastore import Datastore
     from pathlib import Path
-    local_cache_path = Path("./snakemake/storage/datastore")
+    local_cache_path = Path(".snakemake/storage/datastore")
     local_cache_path.mkdir(parents=True, exist_ok=True)
+    # NOTE (2026-08-20): if we decide on cloud cache for Datastore, should
+    # configure that here too.
     ds = Datastore(local_cache_path=local_cache_path)
-    datapackage_descriptor = ds.get_datapackage_descriptor(dataset)
-    resource_names = [key.name for key in datapackage_descriptor.get_resources(**filters)]
-    resource_paths = [datapackage_descriptor.get_resource_path(name) for name in resource_names]
-    return [storage.http(path) for path in resource_paths]
+    resources = ds.get_resources(dataset, **filters)
+    return [local_cache_path / key.get_local_path() for key, _contents in resources]
 
 
 rule supply_curve:
--- src/cnems_inputs/stub_supply_curve.py
+++ src/cnems_inputs/stub_supply_curve.py
@@ -10,15 +10,16 @@ import polars as pl
 from upath import UPath
 
 
-def extract(zip_path: UPath) -> pl.LazyFrame:
-    """Make a LazyFrame from the input path.
+def extract(archive_path: UPath, member_path: str) -> pl.LazyFrame:
+    """Make a LazyFrame from a file within a ZIP archive.
 
-    We want to read the data *at extraction time*, not at definition time, so we get the zip_path as the HTTP path, not a local cached path.
-
-    NOTE (2026-08-19): consider using the cached-http storage plugin!
+    archive_path: path to the ZIP archive itself. Since we're using the
+        Datastore to cache files, this is likely a local path but *could* be a
+        remote path depending on cache layer configuration.
+    member_path: the file name within the ZIP archive.
     """
-    with UPath(zip_path).open("rb") as blob, ZipFile(blob) as zf:
-        content = zf.open("input/electricity/cem_inputs/SupplyCurve.csv")
+    with UPath(archive_path).open("rb") as blob, ZipFile(blob) as zf:
+        content = zf.open(member_path)
         return pl.scan_csv(content)
 
 
@@ -30,25 +31,33 @@ def transform(raw: pl.LazyFrame) -> pl.LazyFrame:
 def load(transformed: pl.LazyFrame, output_path: Path) -> None:
     """Write LazyFrame to output.
 
-    Note that here, we've set the S3 storage plugin to `retrieve=True`. This
-    means that we write to a local cached path, and the S3 storage plugin will read
-    from that path and write to the configured location.
-
-    We use the S3 storage plugin because that lets Snakemake better understand
-    which outputs have been built vs. need rebuilding.
+    transformed: the data we want to write out.
+    output_path: a path for us to write the data out to, which may then be
+        pushed remotely via the storage backend configured for this output.
     """
     transformed.sink_csv(output_path)
 
 
 def run(input_path, output_path):
     """E, T, L."""
-    load(transform(extract(input_path)), output_path)
+    load(
+        transform(
+            extract(
+                archive_path=input_path,
+                member_path="input/electricity/cem_inputs/SupplyCurve.csv",
+            )
+        ),
+        output_path,
+    )
 
 
 if __name__ == "__main__":
     from typing import TYPE_CHECKING
 
     if TYPE_CHECKING:
+        # NOTE (2026-08-20): outside of type checking context, the Snakemake
+        # runtime injects the `snakemake` object, but ruff doesn't know that
+        # so it complains about this import
         from snakemake.iocontainers import snakemake  # noqa: TC004
 
     run(snakemake.input[0], snakemake.output[0])
