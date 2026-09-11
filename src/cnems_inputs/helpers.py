1351fe64093baab6ca92a0558c17520f3950c945
Dazhong Xia <dazhong.xia@catalyst.coop>
2026-09-11 13:55:43 -0400

    chore: add output bucket to config values (#47)

--- src/cnems_inputs/helpers.py
+++ src/cnems_inputs/helpers.py
@@ -7,7 +7,7 @@ import os
 #
 # NOTE (2026-08-28): we *could* do some fancy parsing of the git rev here if we
 # really want, but setting env var in GHA seemed easier
-def versioned_r2_uri(path: str) -> str:
+def versioned_r2_uri(bucket: str, path: str) -> str:
     """Given a path to publish to, generate the right R2 URI.
 
     * correct version prefix - set CNEMS_INPUT_VERSION_ID to publish to a
@@ -15,6 +15,4 @@ def versioned_r2_uri(path: str) -> str:
     * correct bucket
     """
     version = os.getenv("CNEMS_INPUT_VERSION_ID", "nightly")
-    # TODO (2026-09-09): should stop hard-coding once we have multiple buckets
-    bucket = "test-catalyst-coop"
     return f"s3://{bucket}/{version}/{path}"
--- tests/unit/helpers_test.py
+++ tests/unit/helpers_test.py
@@ -5,15 +5,11 @@ from cnems_inputs.helpers import versioned_r2_uri
 
 def test_versioned_r2_uri_env_override(monkeypatch):
     monkeypatch.setenv("CNEMS_INPUT_VERSION_ID", "vTest")
-    assert (
-        versioned_r2_uri("test_path.csv")
-        == "s3://test-catalyst-coop/vTest/test_path.csv"
-    )
+    assert versioned_r2_uri("test", "test_path.csv") == "s3://test/vTest/test_path.csv"
 
 
 def test_versioned_r2_uri_defaults_to_nightly(monkeypatch):
     monkeypatch.delenv("CNEMS_INPUT_VERSION_ID", raising=False)
     assert (
-        versioned_r2_uri("test_path.csv")
-        == "s3://test-catalyst-coop/nightly/test_path.csv"
+        versioned_r2_uri("test", "test_path.csv") == "s3://test/nightly/test_path.csv"
     )
